fc80990fd5685a7d3dd9d63c40e11685ef45199e
hutzu <stefana.hutupasu.p@gmail.com>
2023-12-24 00:44:13 +0200

    created app

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+// src/components/Header.js
+
+import React from 'react';
+import './Header.css'; // Import the CSS file for styling
+
+function Header() {
+  return (
+    <header className="header">
+      {/* Your header content goes here */}
+      <h1>My E-commerce Site</h1>
+    </header>
+  );
+}
+
+export default Header;
